51d8a249912279842715cad1ba09837cea0f7367
Ben Sandbrook <bsandbro@vt.edu>
2023-09-14 16:37:16 +0000

    removing more duplicate code

--- atlas_conversion/convertRAW.py
+++ atlas_conversion/convertRAW.py
@@ -135,49 +135,6 @@ def ImageSlices2TiledImage(filenames, loadImgFunction=loadRAW2Numpy, cGradient=F
     return imout, gradient, size, numberOfSlices, slicesPerAxis
 
 
-# This functions takes a (tiled) image and writes it to a png file with base filename outputFilename.
-# It also writes several versions in different sizes determined by dimensions
-def write_versions(tileImage, tileGradient, outputFilename, dimensions=None):
-    if dimensions is None:
-        dimensions = [8192, 4096, 2048, 1024, 512]
-    try:
-        print('Creating folder', os.path.dirname(outputFilename), '...', end=' ')
-        os.makedirs(os.path.dirname(outputFilename))
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(os.path.dirname(outputFilename)):
-            print('was already there.')
-        else:
-            print(', folders might not be created, trying to write anyways...')
-    except:
-        print("Could not create folders, trying to write anyways...")
-
-    print("Writing complete image: " + outputFilename + "_full.png")
-    try:
-        tileImage.save(outputFilename + "_full.png", "PNG")
-        if tileGradient:
-            tileGradient.save(outputFilename + "_gradient_full.png", "PNG")
-    except:
-        print("Failed writing ", outputFilename + "_full.png")
-    for dim in dimensions:
-        if tileImage.size[0] > dim:
-            print("Writing " + str(dim) + "x" + str(dim) + " version: " + outputFilename + "_" + str(dim) + ".png")
-            try:
-                tmpImage = tileImage.resize((dim, dim))
-                tmpImage.save(outputFilename + "_" + str(dim) + ".png", "PNG")
-            except:
-                print("Failed writing ", outputFilename, "_", str(dim), ".png")
-            if tileGradient:
-                try:
-                    tmpImage = tileGradient.resize((dim, dim))
-                    tmpImage.save(outputFilename + "_gradient_" + str(dim) + ".png", "PNG")
-                except:
-                    print("Failed writing ", outputFilename, "_gradient_", str(dim), ".png")
-
-
-# This function lists the files within a given directory dir
-def listdir_fullpath(d):
-    return [os.path.join(d, f) for f in os.listdir(d)]
-
 
 # This is the main program, it takes at least 2 arguments <InputFolder> and <OutputFilename>
 def main():
