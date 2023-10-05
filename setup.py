50121f5ce28bbd6aeab44104372f3ae3ca045a14
Ben Sandbrook <bsandbro@vt.edu>
2023-10-05 18:46:45 +0000

    remove pandas from reqs, add pydicom

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='atlas_conversion',
     version='0.1.0',
     packages=['atlas_conversion'],
-    install_requires=['pandas',
-                      'numpy',
+    install_requires=['numpy',
                       'scipy',
                       'h5py',
                       'dask',
-                      'pillow'],
+                      'pillow'
+                      'pydicom'],
     entry_points={
         'console_scripts': [
             'atlas_conversion = atlas_conversion.__main__:main'
